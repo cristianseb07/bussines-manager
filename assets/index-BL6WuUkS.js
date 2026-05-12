25f8239a586a81dfd5174a493a2621cba4559a6f
cristianseb07 <91921855+cristianseb07@users.noreply.github.com>
2026-05-12 11:06:51 -0300

    Delete assets/index-BL6WuUkS.js

--- assets/index-BL6WuUkS.js
+++
@@ -1,9 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BevIyEVv.js","./index-D-Ubi8Nr.js"])))=>i.map(i=>d[i]);
-import { _ as __vitePreload } from "./index-BTZOdFbx.js";
-import { r as registerPlugin } from "./index-D-Ubi8Nr.js";
-const App = registerPlugin("App", {
-  web: () => __vitePreload(() => import("./web-BevIyEVv.js"), true ? __vite__mapDeps([0,1]) : void 0, import.meta.url).then((m) => new m.AppWeb())
-});
-export {
-  App
-};
